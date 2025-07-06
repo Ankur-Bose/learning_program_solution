0ed9ce65a1fdad5dc846f0dbbaa875081d4ee42f
Ankur Bose <ankurbose04@gmail.com>
2025-07-06 23:21:17 +0530

    Mandatoy Hands-on

---
+++ Week3_SpringCoreAndMaven/Exercise2_Implementing_Dependency_Injection/LibraryManagementApp/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,18 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        System.out.println("Books from repository:");
+        bookRepository.getAllBooks().forEach(System.out::println);
+    }
+}
